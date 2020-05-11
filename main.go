b969b449a3f8742a0dc7b52a114707101362fb41
James <hotjameschang@qq.com>
2020-05-11 14:38:09 +0800

    add main file for test

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"leet1000/is_happy"
+)
+
+/* for test */
+func main() {
+	n := 100
+
+	//testRes := two_sum.TwoSum(nums, target)
+	is_happy.IsHappy(n)
+
+	//fmt.Println(testRes)
+}
