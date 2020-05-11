81d0770745ed657ace39de0b0481ddcd54b46959
HotJames <hotjameschang@qq.com>
2020-05-11 16:33:23 +0800

    reverse int

---
+++ int_reverse/solution.go
@@ -0,0 +1,35 @@
+package int_reverse
+
+import (
+	"math"
+)
+
+func Reverse(x int) int {
+
+	sign := 1
+
+	if x < 0 {
+		sign = -1
+		x = sign * x
+	}
+
+	y := 0
+
+	for x > 0 {
+
+		temp := x % 10
+
+		y = y*10 + temp
+
+		x = x / 10
+
+	}
+
+	y = sign * y
+
+	if y > math.MaxInt32 || y < math.MinInt32 {
+		return 0
+	}
+
+	return y
+}
