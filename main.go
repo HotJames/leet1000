244c83161fcff747ea1c2ed3d22667311ffeccb9
HotJames <hotjameschang@qq.com>
2020-05-11 16:34:29 +0800

    update

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"leet1000/is_happy"
+	"fmt"
+	"leet1000/int_reverse"
 )
 
 /* for test */
 func main() {
-	n := 100
+	n := 123
 
-	//testRes := two_sum.TwoSum(nums, target)
-	is_happy.IsHappy(n)
-
+	testRes := int_reverse.Reverse(n)
+	//is_happy.IsHappy(n)
 	//fmt.Println(testRes)
+	fmt.Println(testRes)
 }
