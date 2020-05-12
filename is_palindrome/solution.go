cda18969b722b91f43bda80b63bfa47e543432f3
HotJames <hotjameschang@qq.com>
2020-05-12 14:55:44 +0800

    add is_palindrome

---
+++ is_palindrome/solution.go
@@ -0,0 +1,28 @@
+package is_palindrome
+
+import "strconv"
+
+func IsPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	s := strconv.Itoa(x)
+
+	r := []rune(s)
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	y, _ := strconv.Atoi(string(r))
+
+	if x == y {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+/*第二种数组前后位置比较*/
